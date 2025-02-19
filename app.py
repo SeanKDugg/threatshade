05197b50329e7137c6d2575422f1a548b7ea923b
Karolina <karolinapiech1990@gmail.com>
2025-02-19 22:04:38 +0000

    Fix title, connect bootstrap and static files

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static', static_folder='static')
 
 """ 
 Defines the route for index page 
@@ -10,4 +10,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)

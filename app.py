810a8c7c98e47f29c0cda2705900b2c6fbc63783
Karolina <karolinapiech1990@gmail.com>
2025-04-07 20:54:44 +0100

    Add base services page

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
 import json
+from flask import Flask, render_template
 
 
 app = Flask(__name__, static_url_path='/static', static_folder='static')
@@ -12,12 +12,19 @@ with open('text_content.json', encoding="utf-8") as json_file:
     data = json.load(json_file)
 
 
-""" 
+"""
 Defines the route for index page
 """
 @app.route('/')
 def index():
     return render_template('index.html', data=data)
+
+"""
+Defines the route for services page
+"""
+@app.route('/services/')
+def services():
+    return render_template('services.html', data=data)
 
 if __name__ == "__main__":
     app.run(debug=True)

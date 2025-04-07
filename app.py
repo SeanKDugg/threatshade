0f6011f7abff3be62ab9a9dbf6655c72a6b068a1
Karolina <karolinapiech1990@gmail.com>
2025-04-07 19:02:27 +0100

    Add json data to index page

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
 from flask import Flask, render_template
+import json
+
 
 app = Flask(__name__, static_url_path='/static', static_folder='static')
 
+
+"""
+Reads json file with page contents
+"""
+with open('text_content.json', encoding="utf-8") as json_file:
+    data = json.load(json_file)
+
+
 """ 
-Defines the route for index page 
+Defines the route for index page
 """
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', data=data)
 
 if __name__ == "__main__":
     app.run(debug=True)

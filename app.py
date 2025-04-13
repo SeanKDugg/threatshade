f320abb9a47bce8add7659b33fc1ae4bb582c272
Karolina <karolinapiech1990@gmail.com>
2025-04-13 15:57:55 +0100

    Fix json data not being fully accessed

--- app.py
+++ app.py
@@ -12,21 +12,43 @@ with open('text_content.json', encoding="utf-8") as json_file:
     data = json.load(json_file)
 
 """
-Defines the route for index page
+Defines the route for index page and passes json data to template
 """
 @app.route('/')
 def index():
-    return render_template('index.html', index=data['index'][0])
+
+    schedule_assessment = data['index'][0]
+    about = data['index'][1]
+
+    return render_template(
+        'index.html',
+        paragraph1=schedule_assessment['paragraph1'],
+        paragraph2=schedule_assessment['paragraph2'],
+        paragraph3=about['paragraph1']
+    )
 
 """
-Defines the route for services page
+Defines the route for services page and passes json data to template
 """
 @app.route('/services/')
 def services():
-    return render_template('services.html', services=data['services'][0])
+    
+    penetration_testing = data['services'][0]
+    vulnerability_scanning = data['services'][1]
+    threat_modelling = data['services'][2]
+
+    return render_template(
+        'services.html',
+        paragraph1=penetration_testing['paragraph1'],
+        paragraph2=penetration_testing['paragraph2'],
+        paragraph3=vulnerability_scanning['paragraph1'],
+        paragraph4=vulnerability_scanning['paragraph2'],
+        paragraph5=threat_modelling['paragraph1'],
+        paragraph6=threat_modelling['paragraph2']
+    )
 
 """
-Defines the route for contact page
+Defines the route for contact page and passes json data to template
 """
 @app.route('/contact-us/')
 def contact():
